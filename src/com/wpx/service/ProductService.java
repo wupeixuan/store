a0ce05f8c017a210e4163dfa896563a4e68ae327
wupeixuan <18603203910@163.com>
2018-02-28 12:15:47 +0800

    天禾裕商城

--- src/com/wpx/service/ProductService.java
+++ src/com/wpx/service/ProductService.java
@@ -16,4 +16,6 @@ public interface ProductService {
     PageBean<Product> findByPage(int currPage, int pageSize, String cid) throws Exception;
 
     List<Product> findAll() throws Exception;
+
+    void add(Product p) throws Exception;
 }
--- src/com/wpx/service/impl/ProductServiceImpl.java
+++ src/com/wpx/service/impl/ProductServiceImpl.java
@@ -8,59 +8,71 @@ import com.wpx.utils.BeanFactory;
 
 import java.util.List;
 
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
 
-	/**
-	 * 查询最新
-	 */
-	@Override
-	public List<Product> findNew() throws Exception {
-		ProductDao pdao=(ProductDao) BeanFactory.getBean("ProductDao");
-		return pdao.findNew();
-	}
+    /**
+     * 查询最新
+     */
+    @Override
+    public List<Product> findNew() throws Exception {
+        ProductDao pdao = (ProductDao) BeanFactory.getBean("ProductDao");
+        return pdao.findNew();
+    }
 
-	/**
-	 * 查询热门
-	 */
-	@Override
-	public List<Product> findHot() throws Exception {
-		ProductDao pdao=(ProductDao) BeanFactory.getBean("ProductDao");
-		return pdao.findHot();
-	}
+    /**
+     * 查询热门
+     */
+    @Override
+    public List<Product> findHot() throws Exception {
+        ProductDao pdao = (ProductDao) BeanFactory.getBean("ProductDao");
+        return pdao.findHot();
+    }
 
-	/**
-	 * 查询单个商品
-	 */
-	@Override
-	public Product getByPid(String pid) throws Exception {
-		ProductDao pdao=(ProductDao) BeanFactory.getBean("ProductDao");
-		return pdao.getByPid(pid);
-	}
+    /**
+     * 查询单个商品
+     */
+    @Override
+    public Product getByPid(String pid) throws Exception {
+        ProductDao pdao = (ProductDao) BeanFactory.getBean("ProductDao");
+        return pdao.getByPid(pid);
+    }
 
-	/**
-	 * 按类别分页查询商品
-	 */
-	@Override
-	public PageBean<Product> findByPage(int currPage, int pageSize, String cid) throws Exception {
-		ProductDao pdao=(ProductDao) BeanFactory.getBean("ProductDao");
-		//当前页数据
-		List<Product> list=pdao.findByPage(currPage,pageSize,cid);
-		
-		//总条数
-		int totalCount = pdao.getTotalCount(cid);
-		
-		return new PageBean<>(list, currPage, pageSize, totalCount);
-	}
+    /**
+     * 按类别分页查询商品
+     */
+    @Override
+    public PageBean<Product> findByPage(int currPage, int pageSize, String cid) throws Exception {
+        ProductDao pdao = (ProductDao) BeanFactory.getBean("ProductDao");
+        //当前页数据
+        List<Product> list = pdao.findByPage(currPage, pageSize, cid);
+
+        //总条数
+        int totalCount = pdao.getTotalCount(cid);
+
+        return new PageBean<>(list, currPage, pageSize, totalCount);
+    }
 
     /**
      * 查询所有商品
+     *
      * @return
      * @throws Exception
      */
-	@Override
-	public List<Product> findAll() throws Exception {
+    @Override
+    public List<Product> findAll() throws Exception {
         ProductDao pd = (ProductDao) BeanFactory.getBean("ProductDao");
-		return pd.findAll();
-	}
+        return pd.findAll();
+    }
+
+    /**
+     * 添加商品
+     *
+     * @param p
+     * @throws Exception
+     */
+    @Override
+    public void add(Product p) throws Exception {
+
+    }
 
 }
